cfeae865b9925d93ff818c4770c00fb0d1e03fbb
AbiVijayarathinam <abarnaabi970@gmail.com>
2021-11-15 08:58:15 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,24 @@
+# What is this?
+
+Get perfect shadows every time for the non-designer.
+
+# Instalation
+npm i shadoboost --save
+
+Then...
+
+'''
+
+import { shadoboost } from 'shadoboost;
+
+    shadoboost({
+        shadow_type: 'hard',
+        padding: false
+});
+'''
+
+## Options
+shadoboost supports 2 options, oth of which are optional:
+
+* *shadow_type* - hard | soft (Default is soft)
+* *padding* - boolean (Default is false)
---
+++ index.js
@@ -0,0 +1,23 @@
+function shadoboost(options) {
+    let components  = document.querySelectorAll('.shadoboost');
+
+    if (options.shadow_type === "hard") {
+        options.shadow_type = "0px";
+    }
+    else{
+        options.shadow_type = "15px";
+    }
+
+    components.forEach(component => {
+        component.style.boxShadow = `10px 10px ${options.shadow_type} 1px rgba(0,0,0,0.12)`;
+
+        if( options.padding) {
+            component.style.padding= "1em";
+        }
+        else {
+            component.style.padding= "0px";
+        }
+    })
+}
+
+module.exports.shadoboost = shadoboost;
